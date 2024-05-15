e8c9b7c9734239db80ea032c80a8f41ba5ab6668
devGabyAlves <gabriellyalves401@gmail.com>
2024-05-15 13:14:25 -0300

    styles: update page imovel

--- src/components/comodos_componentes.tsx
+++ src/components/comodos_componentes.tsx
@@ -1,13 +1,13 @@
+import React, { useState } from 'react';
 import axios from 'axios';
-import { ChangeEvent, useState } from 'react';
-// import '../styles/styles.css';
+import { Box, Button, Grid, Modal, Stack, TextareaAutosize, TextField, Typography } from '@mui/material';
 import Header from './Header';
 import Menu from './menu';
 
 interface Component {
-  name: string
-  description: string
-  photos: string[]
+  name: string;
+  description: string;
+  photos: string[];
 }
 
 const Componente = () => {
@@ -20,20 +20,21 @@ const Componente = () => {
   ]);
 
   const [novoNomeComponente, setNovoNomeComponente] = useState('');
-  const [fotoModal, setFotoModal] = useState('');
+  const [fotoModal, setFotoModal] = useState<string | null>(null);
 
-  const handleDescricaoChange = (index: number, event: ChangeEvent<HTMLTextAreaElement>) => {
+  const handleDescricaoChange = (index: number, event: React.ChangeEvent<HTMLTextAreaElement>) => {
     const novosComponentes = [...componentes];
     novosComponentes[index].description = event.target.value;
     setComponentes(novosComponentes);
   };
 
-  const handleAdicionarFoto = (index: number, event: ChangeEvent<HTMLInputElement>) => {
+  const handleAdicionarFoto = (index: number, event: React.ChangeEvent<HTMLInputElement>) => {
     const novosComponentes = [...componentes];
-
-    novosComponentes[index].photos.push(URL.createObjectURL(event.target.files![0]));
-
-    setComponentes(novosComponentes);
+    const files = event.target.files;
+    if (files) {
+      novosComponentes[index].photos.push(URL.createObjectURL(files[0]));
+      setComponentes(novosComponentes);
+    }
   };
 
   const handleAdicionarComponente = () => {
@@ -51,7 +52,7 @@ const Componente = () => {
   };
 
   const fecharModal = () => {
-    setFotoModal('');
+    setFotoModal(null);
   };
 
   const handleExtrairRelatorio = () => {
@@ -59,74 +60,83 @@ const Componente = () => {
       userId: 'ID_DO_USUARIO',
       imovelId: 'ID_DO_IMOVEL'
     })
-      .then(response => {
-        console.log('Relatório extraído com sucesso:', response.data);
-      })
-      .catch(error => {
-        console.error('Erro ao extrair relatório:', error);
-      });
+      .then(response => console.log('Relatório extraído com sucesso:', response.data))
+      .catch(error => console.error('Erro ao extrair relatório:', error));
   };
 
   return (
-    <div>
+    <Box>
       <Header />
-      <Menu /> {/* Inclui o menu de hambúrguer */}
-      <div className="flex flex-wrap p-4"> {/* .container */}
+      <Menu />
+      <Grid container spacing={2} sx={{ p: 2 }}>
         {componentes.map((componente, index) => (
-          <div key={index} className="w-1/5 p-5"> {/* .container > div */}
-            <h2>{componente.name}</h2>
-            <div>
-              <label htmlFor={`description-${index}`}>Descrição:</label>
-              <textarea
-                id={`description-${index}`}
-                value={componente.description}
-                onChange={(event) => handleDescricaoChange(index, event)}
-                rows={6}
-                className="w-full mb-4"
+          <Grid item xs={12} sm={6} md={4} key={index}>
+            <Typography variant="h6">{componente.name}</Typography>
+            <TextareaAutosize
+              aria-label={`description-${index}`}
+              minRows={3}
+              value={componente.description}
+              onChange={(event) => handleDescricaoChange(index, event as React.ChangeEvent<HTMLTextAreaElement>)}
+              style={{ width: '100%' }}
+            />
+            <TextField
+              type="file"
+              InputLabelProps={{ shrink: true }}
+              label="Adicionar Foto"
+              variant="outlined"
+              onChange={(event) => handleAdicionarFoto(index, event as React.ChangeEvent<HTMLInputElement>)}
+              fullWidth
+              margin="normal"
+              inputProps={{ multiple: true }}
+            />
+            {componente.photos.map((foto, fotoIndex) => (
+              <img
+                key={fotoIndex}
+                src={foto}
+                alt={`Foto ${fotoIndex + 1}`}
+                style={{ width: '100%', cursor: 'pointer', marginRight: '10px' }}
+                onClick={() => abrirModal(foto)}
               />
-            </div>
-            <div>
-              <label htmlFor={`foto-${index}`}>Adicionar Foto:</label>
-              <input
-                type="file"
-                id={`foto-${index}`}
-                onChange={(event) => handleAdicionarFoto(index, event)}
-                multiple
-                className="mb-4"
-              />
-            </div>
-            <div>
-              {componente.photos.map((foto, fotoIndex) => (
-                <img
-                  key={fotoIndex}
-                  src={foto}
-                  alt={`Foto ${fotoIndex + 1}`}
-                  className="w-50 h-auto mr-2.5 cursor-pointer" /* Adjusted width and margin */
-                  onClick={() => abrirModal(foto)}
-                />
-              ))}
-            </div>
-          </div>
+            ))}
+          </Grid>
         ))}
-        <div className="absolute top-30 right-2.5"> {/* Adjusted positions */}
-          <input
-            type="text"
+        <Grid item xs={12} sm={8} md={4}>
+          <TextField
+            label="Digite novo Cômodo"
+            variant="outlined"
             value={novoNomeComponente}
             onChange={(event) => setNovoNomeComponente(event.target.value)}
-            placeholder="Digite novo Comodo"
-            className="mb-4"
+            fullWidth
+            margin="normal"
           />
-          <button className="mb-4 w-full" onClick={handleAdicionarComponente}>Adicionar Componente</button>
-          <button className="mb-4 w-full" onClick={handleExtrairRelatorio}>Extrair relatório</button>
-        </div>
-        {fotoModal && (
-          <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50" onClick={fecharModal}>
-            <img src={fotoModal} alt="Foto" className="max-w-screen-lg max-h-screen-90" />
-            <button className="absolute top-2.5 right-2.5 bg-white border-none cursor-pointer z-50" onClick={fecharModal}>X</button>
-          </div>
-        )}
-      </div>
-    </div>
+          <Stack direction="row" spacing={1} sx={{ mt: 1 }}>
+            <Button
+              variant="contained"
+              onClick={handleAdicionarComponente}
+              sx={{ backgroundColor: '#673ab7', color: '#fff', '&:hover': { backgroundColor: '#5e35b1' } }}
+              style={{ flex: 1 }}
+            >
+              Add Componente
+            </Button>
+            <Button
+              variant="contained"
+              onClick={handleExtrairRelatorio}
+              sx={{ backgroundColor: '#673ab7', color: '#fff', '&:hover': { backgroundColor: '#5e35b1' } }}
+              style={{ flex: 1 }}
+            >
+              Extrair relatório
+            </Button>
+          </Stack>
+        </Grid>
+      </Grid>
+      <Modal
+        open={!!fotoModal}
+        onClose={fecharModal}
+        sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+      >
+        <img src={fotoModal || ''} alt="Foto Modal" style={{ maxWidth: '90%', maxHeight: '90%' }} />
+      </Modal>
+    </Box>
   );
 };
 
