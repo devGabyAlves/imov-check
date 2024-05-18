8fc9c8a4c6f9a2bbd08b29dff088366093a2a065
DiogoJorge1401 <diogojorge1401@gmail.com>
2024-05-17 21:19:11 -0300

    chore: Update Vite plugins in package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import million from "million/compiler";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
 
 export default defineConfig({
   plugins: [million.vite({ auto: true }), react()],
