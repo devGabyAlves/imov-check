a7f98b130906b70e85ab4afda118b02cd165b405
DiogoJorge1401 <diogojorge1401@gmail.com>
2024-05-23 23:37:01 -0300

    feat: Update routing configuration for private routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Componente from "./components/comodos_componentes";
-import FormCadastro from "./components/form_cadastro";
-import FormCorretor from "./components/form_cadastro_corretor";
-import Home from "./components/home"
-import Login from "./components/login";
-import Pesquisa from "./components/pesquisa";
-
-// const PrivateRoute = ({ element, ...rest }) => {
-//   const isLoggedIn = localStorage.getItem('token');
-//   return isLoggedIn ? <Route {...rest} element={element} /> : <Navigate to="/" replace />;
-// };
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Componente from './components/comodos_componentes';
+import FormCadastro from './components/form_cadastro';
+import FormCorretor from './components/form_cadastro_corretor';
+import Home from './components/home';
+import Login from './components/login';
+import Pesquisa from './components/pesquisa';
+import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
   return (
@@ -17,10 +13,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/imoveis" element={<Componente />} />
-        <Route path="/cadastro_imovel" element={<FormCadastro />} />
-        <Route path="/cadastro_corretor" element={<FormCorretor />} />
-        <Route path="/pesquisa" element={<Pesquisa />} />
+        <Route path="/imoveis" element={<PrivateRoute element={Componente} />} />
+        <Route path="/cadastro_imovel" element={<PrivateRoute element={FormCadastro} />} />
+        <Route path="/cadastro_corretor" element={<PrivateRoute element={FormCorretor} />} />
+        <Route path="/pesquisa" element={<PrivateRoute element={Pesquisa} />} />
       </Routes>
     </Router>
   );
